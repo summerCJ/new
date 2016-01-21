d5f6bff052e64b37ae8d409b1c663a0c44507970
summerCJ <360813931@qq.com>
2016-01-21 11:06:15 +0800

    print hello world

---
+++ HelloWorldDemo/src/cc/java/HelloWorldDemo.java
@@ -0,0 +1,21 @@
+package cc.java;
+
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+		System.out.println("hello,world");
+		System.out.println();
+		System.out.println();
+		class A{
+			int a = 2;
+			static int b = 4;
+			
+			
+		}
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
